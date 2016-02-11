964a3888f2462deb9496210687e82f896cb752d9
laurensWe <laurensweijs@hotmail.com>
2016-02-11 20:23:58 +0100

    ..

--- src/Order.java
+++ src/Order.java
@@ -1,5 +1,17 @@
+import java.util.ArrayList;
+
 /**
  * Created by laure on 2/11/2016.
  */
 public class Order {
+    private Cell orderDestination;
+    private int[] stillNeeded;
+    private boolean isCompleted;
+    private int turnCompleted;
+    private ArrayList<ArrayList<Product>> productReceived;
+    private int[] originalRequest;
+
+    public Order(){
+        isCompleted = false;
+    }
 }
